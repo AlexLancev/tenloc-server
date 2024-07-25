4f3ee3ac835a74c658b9435ea6db834f3664c25f
Aleksei Vavulo <aleksei.lancevv@gmail.com>
2024-07-25 22:46:57 +0500

    update database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const url_api = process.env.URL;
 
 app.use(express.json());
 
@@ -10,8 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", require("./routes/planes"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/database").then(() => {
-  app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+mongoose
+  .connect(url_api, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
   });
-});
